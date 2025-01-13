2f1b819c2a71d376d65c6a4af918729e59c54b0e
Bilalmanna16 <bilalmanna16@gmail.com>
2025-01-13 16:19:05 +0530

    bug fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -368,7 +368,8 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>{
                 },
                 isSubscribed: {
                     $cond: {
-                        if: {$in: [req.user?._id,"$subscribers.subscriber"]},
+                        if: {$in: [req.user?._id, "$subscribers.subscriber"]
+                        },
                         then: true,
                         else: false
                     }
